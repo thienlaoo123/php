65f2fd4171ef36badeb0dd22785282082ef25a9b
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-06 01:05:32 +0300

    Update index.php

--- lab3 /site/index.php
+++ lab3 /site/index.php
@@ -1,1 +1,42 @@
 
+<?php
+    require_once 'inc/lib.inc.php';
+    require_once 'inc/data.inc.php';
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Сайт нашей школы</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <!-- Верхняя часть страницы -->
+    <?php include 'inc/top.inc.php'; ?>
+    <!-- Верхняя часть страницы -->
+  </header>
+
+  <section>
+    <!-- Заголовок -->
+    <h1>Добро пожаловать на наш сайт!</h1>
+    <!-- Заголовок -->
+    <!-- Область основного контента -->
+    <?php include 'inc/index.inc.php'; ?>
+    <!-- Область основного контента -->
+  </section>
+  <nav>
+    <!-- Навигация -->
+    <?php include 'inc/menu.inc.php'; ?>
+    <!-- Навигация -->
+  </nav>
+  <footer>
+    <!-- Нижняя часть страницы -->
+    <?php include 'inc/bottom.inc.php'; ?>
+    <!-- Нижняя часть страницы -->
+  </footer>
+</body>
+</html>
