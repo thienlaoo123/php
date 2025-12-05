09adbb914f135dd37f85dbb31ba58616f1ca8aa1
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-06 01:53:50 +0300

    Update SuperUser.php

--- lab7/MyProject/Classes/SuperUser.php
+++ lab7/MyProject/Classes/SuperUser.php
@@ -1,1 +1,25 @@
+<?php
+namespace MyProject\Classes;
 
+class SuperUser extends User {
+    public $role;
+
+    public function __construct(string $name, string $login, string $password, string $role) {
+        parent::__construct($name, $login, $password);
+        $this->role = $role;
+    }
+
+    /**
+     * Выводит информацию о суперпользователе
+     */
+    public function showInfo(): void {
+        echo "<div style='border: 2px solid #f00; padding: 10px; margin: 10px; background: #ffe6e6;'>";
+        echo "<h3>Информация о СУПЕРпользователе:</h3>";
+        echo "<p><strong>Имя:</strong> {$this->name}</p>";
+        echo "<p><strong>Логин:</strong> {$this->login}</p>";
+        echo "<p><strong>Пароль:</strong> ******</p>";
+        echo "<p><strong>Роль:</strong> {$this->role}</p>";
+        echo "</div>";
+    }
+}
+?>
