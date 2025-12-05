79c034f0e9145c3bea0607baefe3499699e844d9
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-06 01:54:15 +0300

    Create User.php

---
+++ lab7/MyProject/Classes/User.php
@@ -0,0 +1,35 @@
+<?php
+namespace MyProject\Classes;
+
+class User {
+    public $name;
+    public $login;
+    private $password;
+
+    public function __construct(string $name, string $login, string $password) {
+        $this->name = $name;
+        $this->login = $login;
+        $this->password = $password;
+        echo "Пользователь {$this->login} создан<br>";
+    }
+
+    /**
+     * Выводит информацию о пользователе
+     */
+    public function showInfo(): void {
+        echo "<div style='border: 1px solid #ccc; padding: 10px; margin: 10px;'>";
+        echo "<h3>Информация о пользователе:</h3>";
+        echo "<p><strong>Имя:</strong> {$this->name}</p>";
+        echo "<p><strong>Логин:</strong> {$this->login}</p>";
+        echo "<p><strong>Пароль:</strong> ******</p>";
+        echo "</div>";
+    }
+
+    /**
+     * Деструктор класса User
+     */
+    public function __destruct() {
+        echo "Пользователь {$this->login} удален<br>";
+    }
+}
+?>
