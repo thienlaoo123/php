bd2ae83ceeaf058f28274231149e569f40fd9fe8
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-08 18:58:51 +0300

    Update config-sample.php

--- lab8/config-sample.php
+++ lab8/config-sample.php
@@ -1,1 +1,8 @@
+<?php
+// Пример файла конфигурации базы данных.
 
+define('DB_NAME',     'your_database_name');
+define('DB_USER',     'your_db_user');
+define('DB_PASSWORD', 'your_db_password');
+define('DB_HOST',     'localhost');
+define('DB_CHARSET',  'utf8mb4');
