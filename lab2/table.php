4b8564b56e9ded387ad9001d49d031cc295b3631
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-08 18:42:10 +0300

    Update table.php

--- lab2/table.php
+++ lab2/table.php
@@ -1,73 +1,87 @@
 <?php
 declare(strict_types=1);
 
-function createTableDimensions(): array
-{
-    $cols = 8;
-    $rows = 6;
-    return ['cols' => $cols, 'rows' => $rows];
-}
 
-function generateMultiplicationTable(int $cols, int $rows): string
-{
-    $html = '<table>';
-    
-    for ($i = 1; $i <= $rows; $i++) {
-        $html .= '<tr>';
-        for ($j = 1; $j <= $cols; $j++) {
-            
-            if ($i === 1 || $j === 1) {
-                $html .= "<th>" . ($i * $j) . "</th>";
-            } else {
-                $html .= "<td>" . ($i * $j) . "</td>";
-            }
-        }
-        $html .= '</tr>';
-    }
-    
-    $html .= '</table>';
-    return $html;
-}
+/**
+ * @var int $cols Количество столбцов в таблице умножения
+ * @var int $rows Количество строк в таблице умножения
+ */
+$cols = 8;
+$rows = 6;
 
-
-$dimensions = createTableDimensions();
-$cols = $dimensions['cols'];
-$rows = $dimensions['rows'];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Таблица умножения</title>
-    <style>
-        table {
-            border: 2px solid black;
-            border-collapse: collapse;
-        }
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Таблица умножения</title>
+	<style>
+		table {
+			border: 2px solid black;
+			border-collapse: collapse;
+		}
 
-        th,
-        td {
-            padding: 10px;
-            border: 1px solid black;
-            text-align: center;
-        }
+		th,
+		td {
+			padding: 10px;
+			border: 1px solid black;
+			text-align: center;
+		}
 
-        th {
-            background-color: yellow;
-            font-weight: bold;
-        }
-        
-        td {
-            background-color: white;
-        }
-    </style>
+		th {
+			background-color: yellow;
+		}
+
+		.header-cell {
+			background-color: #ffeb3b;
+			font-weight: bold;
+			text-align: center;
+		}
+
+		.data-cell {
+			background-color: white;
+		}
+	</style>
 </head>
 <body>
-    <h1>Таблица умножения</h1>
-    <?php
-    echo generateMultiplicationTable($cols, $rows);
-    ?> 
+	<h1>Таблица умножения</h1>
+	
+	<table>
+		<?php
+		/**
+		 * Отрисовка таблицы умножения
+		 * 
+		 * Используются вложенные циклы for для создания строк и столбцов
+		 * Первая строка и первый столбец выделены как заголовочные
+		 */
+		for ($i = 1; $i <= $rows; $i++): 
+		?>
+			<tr>
+				<?php for ($j = 1; $j <= $cols; $j++): ?>
+					<?php
+					/**
+					 * Определение класса ячейки
+					 * 
+					 * @var string $cellClass Класс для стилизации ячейки
+					 */
+					$cellClass = ($i === 1 || $j === 1) ? 'header-cell' : 'data-cell';
+					
+					/**
+					 
+					 * @var int $cellValue Произведение номера строки и номера столбца
+					 */
+					$cellValue = $i * $j;
+					?>
+					
+					<td class="<?= $cellClass ?>">
+						<?= $cellValue ?>
+					</td>
+				<?php endfor; ?>
+			</tr>
+		<?php endfor; ?>
+	</table>
+	
 </body>
 </html>
