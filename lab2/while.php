e1d7b20da3cc08e823742bd4d6fe59108c898942
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-08 18:42:27 +0300

    Update while.php

--- lab2/while.php
+++ lab2/while.php
@@ -1,31 +1,18 @@
 <?php
 declare(strict_types=1);
 
-    function printStringVertically(string $str): void
-    {
-        $i = 0;
-        $length = strlen($str);
-        
-        while ($i < $length) {
-            echo $str[$i] . "<br>";
-            $i++;
-        }
-    }
 
-    $var = "HELLO";
+// header('Content-Type: text/html; charset=UTF-8');
+
+
+$var = 'ПРИВЕТ';
+
+
+$i = 0;
+
+// Цикл while для вывода символов в столбик
+while ($i < mb_strlen($var)) {
+    echo mb_substr($var, $i, 1) . "<br>";
+    $i++;
+}
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Цикл while</title>
-</head>
-<body>
-    <h1>Цикл while</h1>
-    <?php
-    printStringVertically($var);
-    ?> 
-</body>
-</html>
