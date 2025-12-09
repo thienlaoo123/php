a5d35386efa5130fecd20bad3de15259ad44253e
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-09 11:25:22 +0300

    Update config.php

--- lab8/config.php
+++ lab8/config.php
@@ -1,8 +1,1 @@
-<?php
-// Пример файла конфигурации базы данных.
 
-define('DB_NAME',     'f1172011_login_db');
-define('DB_USER',     'f1172011_login_db');
-define('DB_PASSWORD', 'r3U2pXTUSQ94NU');
-define('DB_HOST',     'localhost');
-define('DB_CHARSET',  'utf8mb4');
