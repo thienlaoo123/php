2b9c7e9b5b153a12ef9dda79bb26dd28c3edd334
thienlaoo123 <borisovalexandr8@gmail.com>
2025-12-11 17:37:04 +0300

    Update index.php

--- lab3 /site/index.php
+++ lab3 /site/index.php
@@ -1,42 +1,24 @@
-
-<?php
-    require_once 'inc/lib.inc.php';
-    require_once 'inc/data.inc.php';
-
+<?php declare(strict_types=1);
+require_once __DIR__ . '/inc/lib.inc.php';
+require_once __DIR__ . '/inc/data.inc.php';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Сайт нашей школы</title>
-  <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сайт нашей школы</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <header>
-    <!-- Верхняя часть страницы -->
-    <?php include 'inc/top.inc.php'; ?>
-    <!-- Верхняя часть страницы -->
-  </header>
-
-  <section>
-    <!-- Заголовок -->
-    <h1>Добро пожаловать на наш сайт!</h1>
-    <!-- Заголовок -->
-    <!-- Область основного контента -->
-    <?php include 'inc/index.inc.php'; ?>
-    <!-- Область основного контента -->
-  </section>
-  <nav>
-    <!-- Навигация -->
-    <?php include 'inc/menu.inc.php'; ?>
-    <!-- Навигация -->
-  </nav>
-  <footer>
-    <!-- Нижняя часть страницы -->
-    <?php include 'inc/bottom.inc.php'; ?>
-    <!-- Нижняя часть страницы -->
-  </footer>
+<!-- Верхняя часть страницы -->
+<?php include_once __DIR__ . '/inc/top.inc.php'; ?>
+<!-- Навигация -->
+<?php include_once __DIR__ . '/inc/menu.inc.php'; ?>
+<!-- Область основного контента -->
+<?php include_once __DIR__ . '/inc/index.inc.php'; ?>
+<!-- Нижняя часть страницы -->
+<?php include_once __DIR__ . '/inc/bottom.inc.php'; ?>
 </body>
 </html>
